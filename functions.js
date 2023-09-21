12ad63f44cc9c61260befa0bd9ed9a84e35a0116
cmdblock <119441799+Commandblock644@users.noreply.github.com>
2023-09-20 17:47:57 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function abcloak(site,iconurl,title) { // ab (about:blank) cloaking stops websites from being shown in history, this also lets you change the site it tries to looklike 
+var abcloak = function(site,iconurl,title) { // ab (about:blank) cloaking stops websites from being shown in history, this also lets you change the site it tries to looklike 
     win = window.open();
     win.document.title = title;
     link = win.document.createElement('link');
